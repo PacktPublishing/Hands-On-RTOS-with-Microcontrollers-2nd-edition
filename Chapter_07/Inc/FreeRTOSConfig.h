b99833f30b3f1cc9e0ae7d7a660afbafc3cc7fd1
Brian Amos <amos@sensornetworksinc.com>
2024-06-28 03:34:53 -0400

    updating configuration to reduce number of priorities and enable task notifications

--- Chapter_07/Inc/FreeRTOSConfig.h
+++ Chapter_07/Inc/FreeRTOSConfig.h
@@ -60,7 +60,7 @@
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
-#define configMAX_PRIORITIES                     ( 56 )
+#define configMAX_PRIORITIES                     ( 4 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configTOTAL_HEAP_SIZE                    ((size_t)15360)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
@@ -98,6 +98,7 @@ to exclude the API function. */
 #define INCLUDE_eTaskGetState   			1
 #define INCLUDE_pxTaskGetStackStart			1
 #define INCLUDE_xTaskGetIdleTaskHandle		1
+#define configUSE_TASK_NOTIFICATIONS        1
 
 
 /* Run time and task stats gathering related definitions. */
