15e6cbf0b35dcf2dcbf0d2f5d252bc4435459b32
Kushal Sharma <45462280+kushal499@users.noreply.github.com>
2025-04-30 20:23:50 +0530

    Delete Common directory

--- Common/Inc/lookBusy.h
+++
@@ -1,8 +0,0 @@
-/*
- Title:  lookBusy header-file
-
- * Copyright (c) by PACKT, 2022, under the MIT License.
- - See the code-repository's license statement for more information:
- - https://github.com/PacktPublishing/Hands-On-RTOS-with-Microcontrollers-2nd-edition
-*/
-void lookBusy( float ms_to_spin );
